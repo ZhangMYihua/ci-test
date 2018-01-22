ba22b7ef63df43ebb61f8f51a165fcd1a3afc903
Yihua Zhang <michaelzhang2@msn.com>
2018-01-22 12:54:37 -0500

    set karma to run once

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     customLaunchers: {
       MyChromeHeadless: {
         base: 'ChromeHeadless',
